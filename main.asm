6296c78c2f71b26c06dfcb2b804ef0d05f0292b5
MrValdez <shunyvaldez@gmail.com>
2014-01-24 23:15:48 +0800

    .

--- main.asm
+++ main.asm
@@ -6,10 +6,10 @@
 ;    .byte $00, $00
 ;    .byte $00,$00,$00,$00,$00,$00,$00,$00
 
-;    .inesprg 1
-;    .ineschr 1
-;    .inesmap 0
-;    .inesmir 1
+    .inesprg 1
+    .ineschr 1
+    .inesmap 0
+    .inesmir 1
 
 ;;;;
 
@@ -19,31 +19,73 @@
 RESET:
     SEI
     CLD
-    
+    LDX #$40
+    STX $4017    ; disable APU frame IRQ
+    LDX #$FF
+    TXS          ; Set up stack
+    INX          ; now X = 0
+    STX $2000    ; disable NMI
+    STX $2001    ; disable rendering
+    STX $4010    ; disable DMC IRQs    
+
 vblankwait1:
     BIT $2002
     BPL vblankwait1
 
 clearmem:
     LDA #$00
-    STA $0000, x
-    STA $0100, x
-    STA $0300, x
-    INX
-    BNE clearmem
 
 vblankwait2:
     BIT $2002
     BPL vblankwait2
     
+;;;;;;;;;;;    
+LoadPalettes:
+    LDA $2002    ; read PPU status to reset the high/low latch
+    LDA #$3F
+    STA $2006    ; write the high byte of $3F00 address
+    LDA #$00
+    STA $2006    ; write the low byte of $3F00 address
+    LDX #$00
+
+LoadPalettesLoop:
+    LDA palette, x        ;load palette byte
+    STA $2007             ;write to PPU
+    INX                   ;set index to next byte
+    CPX #$20            
+    BNE LoadPalettesLoop  ;if x = $20, 32 bytes copied, all done
+;;;;;;;;;;;;    
+
+  LDA #$80
+  STA $0200        ; put sprite 0 in center ($80) of screen vert
+  STA $0203        ; put sprite 0 in center ($80) of screen horiz
+  LDA #$00
+  STA $0201        ; tile number = 0
+  STA $0202        ; color = 0, no flipping
+
+  LDA #%10000000   ; enable NMI, sprites from Pattern Table 0
+  STA $2000
+
+  LDA #%00010000   ; enable sprites
+  STA $2001
+
+    
 InfiniteLoop:
     JMP InfiniteLoop
     
 NMI:
+    LDA #$00
+    STA $2003  ; set the low byte (00) of the RAM address
+    LDA #$02
+    STA $4014  ; set the high byte (02) of the RAM address, start the transfer
     RTI
        
 ;; Vectors
     .bank 1
+    .org $E000
+palette:
+    .db $0F,$31,$32,$33,$0F,$35,$36,$37,$0F,$39,$3A,$3B,$0F,$3D,$3E,$0F
+    .db $0F,$1C,$15,$14,$0F,$02,$38,$3C,$0F,$1C,$15,$14,$0F,$02,$38,$3C
     .org $FFFA
     .dw NMI
     .dw RESET
