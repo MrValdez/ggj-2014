6d382e26f64f4d7ac2d6b3f6eb2c6b076751c3d8
MrValdez <shunyvaldez@gmail.com>
2014-01-25 17:21:56 +0800

    added animation with all sprites of meta-sprite

--- main.asm
+++ main.asm
@@ -179,13 +179,21 @@ AnimatePlayer:
     RTS
     
 AnimatePlayer_Frame1:
-    ; area 1
+    ; segment 1
     LDA #$42
     STA SPRITE_RAM + 1
     
     LDA SPRITE_RAM + 2
     AND #%10111111
     STA SPRITE_RAM + 2 
+
+    ; segment 2
+    LDA #$43
+    STA SPRITE_RAM + 1 + 4
+    
+    LDA SPRITE_RAM + 2 + 4
+    AND #%10111111
+    STA SPRITE_RAM + 2 + 4
 
     ; segment 3
     LDA #$52
@@ -195,15 +203,32 @@ AnimatePlayer_Frame1:
     AND #%10111111
     STA SPRITE_RAM + 10
 
+    ; segment 4
+    LDA #$53
+    STA SPRITE_RAM + 9 + 4
+    
+    LDA SPRITE_RAM + 10 + 4
+    AND #%10111111
+    STA SPRITE_RAM + 10 + 4
+
     RTS
 
 AnimatePlayer_Frame2:
+    ; segment 1
     LDA #$43
     STA SPRITE_RAM + 1
 
     LDA SPRITE_RAM + 2
     ORA #%01000000    
     STA SPRITE_RAM + 2    
+
+    ; segment 2
+    LDA #$42
+    STA SPRITE_RAM + 1 + 4
+
+    LDA SPRITE_RAM + 2 + 4
+    ORA #%01000000    
+    STA SPRITE_RAM + 2 + 4
     
     ; segment 3
     LDA #$53
@@ -212,6 +237,13 @@ AnimatePlayer_Frame2:
     LDA SPRITE_RAM + 10
     ORA #%01000000
     STA SPRITE_RAM + 10
+
+    LDA #$52
+    STA SPRITE_RAM + 9 + 4
+
+    LDA SPRITE_RAM + 10 + 4
+    ORA #%01000000
+    STA SPRITE_RAM + 10 + 4
 
     LDX #$00
     STX player_animation
