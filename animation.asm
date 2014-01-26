7962975d9b1c150c7bcd25752923ab6ef64bbccd
MrValdez <shunyvaldez@gmail.com>
2014-01-26 11:10:43 +0800

    added animation (forgot to commit this earlier)

--- animation.asm
+++ animation.asm
@@ -1,3 +1,147 @@
+AnimatePlayer_Frame1_forme1:
+    ; segment 1
+    LDA #$2C
+    STA SPRITE_RAM + 1
+    
+    LDA SPRITE_RAM + 2
+    AND #%10111111
+    STA SPRITE_RAM + 2
+
+    ; segment 2
+    LDA #$2D
+    STA SPRITE_RAM + 1 + 4
+    
+    LDA SPRITE_RAM + 2 + 4
+    AND #%10111111
+    STA SPRITE_RAM + 2 + 4
+
+    ; segment 3
+    LDA #$3C
+    STA SPRITE_RAM + 9
+    
+    LDA SPRITE_RAM + 10
+    AND #%10111111
+    STA SPRITE_RAM + 10
+
+    ; segment 4
+    LDA #$3D
+    STA SPRITE_RAM + 9 + 4
+    
+    LDA SPRITE_RAM + 10 + 4
+    AND #%10111111
+    STA SPRITE_RAM + 10 + 4
+
+    LDX #$00
+    STX player_animation
+
+    RTS
+
+AnimatePlayer_Frame2_forme1:
+    ; segment 1
+    LDA #$2D
+    STA SPRITE_RAM + 1
+
+    LDA SPRITE_RAM + 2
+    ORA #%01000000    
+    STA SPRITE_RAM + 2    
+
+    ; segment 2
+    LDA #$2C
+    STA SPRITE_RAM + 1 + 4
+
+    LDA SPRITE_RAM + 2 + 4
+    ORA #%01000000    
+    STA SPRITE_RAM + 2 + 4
+    
+    ; segment 3
+    LDA #$3D
+    STA SPRITE_RAM + 9
+
+    LDA SPRITE_RAM + 10
+    ORA #%01000000
+    STA SPRITE_RAM + 10
+
+    LDA #$3C
+    STA SPRITE_RAM + 9 + 4
+
+    LDA SPRITE_RAM + 10 + 4
+    ORA #%01000000
+    STA SPRITE_RAM + 10 + 4
+
+    RTS
+
+AnimatePlayer_Frame1_forme2:
+    ; segment 1
+    LDA #$2E
+    STA SPRITE_RAM + 1
+    
+    LDA SPRITE_RAM + 2
+    AND #%10111111
+    STA SPRITE_RAM + 2
+
+    ; segment 2
+    LDA #$2F
+    STA SPRITE_RAM + 1 + 4
+    
+    LDA SPRITE_RAM + 2 + 4
+    AND #%10111111
+    STA SPRITE_RAM + 2 + 4
+
+    ; segment 3
+    LDA #$3E
+    STA SPRITE_RAM + 9
+    
+    LDA SPRITE_RAM + 10
+    AND #%10111111
+    STA SPRITE_RAM + 10
+
+    ; segment 4
+    LDA #$3F
+    STA SPRITE_RAM + 9 + 4
+    
+    LDA SPRITE_RAM + 10 + 4
+    AND #%10111111
+    STA SPRITE_RAM + 10 + 4
+
+    LDX #$00
+    STX player_animation
+
+    RTS
+
+AnimatePlayer_Frame2_forme2:
+    ; segment 1
+    LDA #$2F
+    STA SPRITE_RAM + 1
+
+    LDA SPRITE_RAM + 2
+    ORA #%01000000    
+    STA SPRITE_RAM + 2    
+
+    ; segment 2
+    LDA #$2E
+    STA SPRITE_RAM + 1 + 4
+
+    LDA SPRITE_RAM + 2 + 4
+    ORA #%01000000    
+    STA SPRITE_RAM + 2 + 4
+    
+    ; segment 3
+    LDA #$3F
+    STA SPRITE_RAM + 9
+
+    LDA SPRITE_RAM + 10
+    ORA #%01000000
+    STA SPRITE_RAM + 10
+
+    LDA #$3E
+    STA SPRITE_RAM + 9 + 4
+
+    LDA SPRITE_RAM + 10 + 4
+    ORA #%01000000
+    STA SPRITE_RAM + 10 + 4
+
+    RTS
+
 AnimatePlayer_Frame1_forme3:
     ; segment 1
     LDA #$40
